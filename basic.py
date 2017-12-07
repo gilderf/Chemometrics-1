8ef6f3f6bbff267a0605eaa60910430d5a7e00b1
litao <believeyour@qq.com>
2017-12-07 16:59:56 +0800

    wip

--- basic.py
+++ basic.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import re
 import jcamp
-PPM = 1e-6
+
 
 def merge_csv(flist, **kwargs):
     """
@@ -27,39 +27,6 @@ def pload(file_name):
     # 读取数据
     with open(file_name, 'rb') as f:
         return pickle.load(f)
-
-
-def avg_mass(mass, delta=20*PPM, min_intensity=0):
-    """
-    平均色谱图
-    :param mass: dataframe columns = ['mz','intensity','rt']
-    :param delta:     离子峰容差
-    :param min_intensity: 最小峰强度
-    :return: 平均色谱图
-    """
-    mass = mass.sort_values(by='mz')
-    mass['cat'] = (mass.mz.diff() > mass.mz*delta).cumsum()
-    group = mass.groupby('cat')
-    mz = group.apply(lambda x: x.mz.dot(x.intensity / x.intensity.sum()))
-    mz.name = 'mz'
-    intensity = group['intensity'].mean()
-    avg = pd.concat([mz, intensity], axis=1)
-    min_mask = avg.intensity / avg.intensity.max() > min_intensity  # 小峰过滤
-    avg = avg.loc[min_mask]
-    return avg
-
-
-def rm_isotopes(mass, delta=1.1):
-    """
-    去除平均质谱的同位素峰
-    :param mass: 平均质谱图 dataframe columns = ['mz','intensity']
-    :param delta: 同位素质量差
-    :return: 去除同位素后的质谱图
-    """
-    mass['cat1'] = (mass['mz'].diff() > delta).cumsum()
-    groups = mass.groupby('cat1').agg('idxmax')
-    mass_rm_isotopes = mass.loc[groups.intensity, ['mz', 'intensity']]
-    return mass_rm_isotopes
 
 
 def get_real(rt):
--- mass_utils.py
+++ mass_utils.py
@@ -12,6 +12,7 @@ def avg_mass(mass, delta=20*ppm, min_intensity=0):
     :param min_intensity: 最小峰强度
     :return: 平均质谱图
     """
+
     mass = mass.sort_values(by='mz')
     mass['cat'] = (mass.mz.diff() > mass.mz*delta).cumsum()
     group = mass.groupby('cat')
@@ -20,7 +21,7 @@ def avg_mass(mass, delta=20*ppm, min_intensity=0):
     rt = group.apply(lambda x: x.rt.dot(x.intensity / x.intensity.sum()))
     rt.name = 'rt'
     intensity = group['intensity'].mean()
-    avg = pd.concat([mz, intensity,rt], axis=1)
+    avg = pd.concat([mz, intensity, rt], axis=1)
     min_mask = avg.intensity / avg.intensity.max() > min_intensity  # 小峰过滤
     avg = avg.loc[min_mask]
     return avg
